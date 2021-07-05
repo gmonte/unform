5acedcb622b443508fb600e6e837e197316e9e38
Guilherme Monte <gmonte@inf.ufsm.br>
2021-07-05 18:06:10 -0300

    created bundle

--- packages/core/typings/useField.d.ts
+++ packages/core/typings/useField.d.ts
@@ -3,6 +3,6 @@ export declare function useField(name: string): {
     registerField: <T>(field: import("./types").UnformField<T>) => void;
     defaultValue: any;
     clearError: () => void;
-    error: string | undefined;
+    error: any;
 };
 //# sourceMappingURL=useField.d.ts.map
